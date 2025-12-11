958502e8c4c26e15a8bbade613aaef1227881dbb
103031430 <103031430@student.swin.edu.au>
2025-12-11 13:07:20 +1100

    fix comment in code

--- src/main.cpp
+++ src/main.cpp
@@ -40,8 +40,8 @@ void CreateSubscriber();
 // Network Configuration
 byte esp_mac[] = { 0xDE, 0xAD, 0xAF, 0x91, 0x3E, 0xD7 };    // Mac address of ESP32
 IPAddress esp_ip(192, 168, 0, 200);                         // IP address of ESP32
-IPAddress dns(192, 168, 0, 1);                         // IP address of ESP32
-IPAddress gateway(192, 168, 0, 1);                         // IP address of ESP32
+IPAddress dns(192, 168, 0, 1);                              // DNS Server (Modify if necessary)
+IPAddress gateway(192, 168, 0, 1);                          // Default Gateway (Modify if necessary)
 IPAddress agent_ip(192, 168, 0, 10);                        // IP address of Micro ROS agent        
 size_t agent_port = 8888;                                   // Micro ROS Agent Port Number
 
